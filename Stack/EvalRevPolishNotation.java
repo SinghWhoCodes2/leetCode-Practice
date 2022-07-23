c0c5b0bd3a100affe157a7627907e94a9ac8e767
singh nav <13designernav@gmail.com>
2022-07-23 14:16:45 -0400

    medium difficulty

---
+++ Stack/EvalRevPolishNotation.java
@@ -0,0 +1,34 @@
+package Stack;
+
+import java.util.Stack;
+
+public class EvalRevPolishNotation {
+	 public int evalRPN(String[] tokens) {
+		 int a,b;
+	        Stack<Integer> stack= new Stack<>();
+	        for (String s : tokens) {
+				
+	        	if (s.equals("+")) {
+					stack.add(stack.pop()+stack.pop());
+				}else if (s.equals("/")) {
+					b=stack.pop();
+					a=stack.pop();
+					stack.add(a/b);
+					
+				}else if (s.equals("*")) {
+					stack.add(stack.pop()*stack.pop());
+				}else if (s.equals("-")) {
+					b=stack.pop();
+					a=stack.pop();
+					stack.add(a-b);
+				}else {
+					stack.add(Integer.parseInt(s));
+				}
+	        		
+			}
+	        		
+	 return stack.pop();
+	 }
+	
+
+}
